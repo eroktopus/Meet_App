e2829ed4fefe89a3b79b9aabbbf5398941e7dd10
eroktopus <147647841+eroktopus@users.noreply.github.com>
2024-04-12 23:17:39 -0400

    OOP

--- src/components/Event.js
+++ src/components/Event.js
@@ -22,7 +22,7 @@ const Event = ({ event }) => {
         {showDetails ? "Hide Details" : "Show Details"}
       </button>
       {showDetails && (
-        <div className="details-section">Details section content goes here</div>
+       <div className="details-section">{event.description}</div>
       )}
     </li>
   );
